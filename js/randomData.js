47b8e6bff726753333309171ce6b4129b5e06024
Michael Depies <depiesms22@uww.edu>
2016-03-02 18:30:48 -0600

    Added some comments to randomData to help clarify behavior. Also adjusted it to work with the jsml.header object when constructing a new data.js object.

--- js/randomData.js
+++ js/randomData.js
@@ -4,17 +4,35 @@ config is expecting these properties
 	header - the header information for each record
 */
 randomData = function(config) {
-	
+	//construct dataset
 	var d = data();
+	//locally store header info from config
 	var _header = config.header;
+	//locally store num of records to generate form config
 	var num_records = config.num_records;
-	d.header(header(_header));
+	
+	//constructing dataset's header via the jsml.header() function
+	d.header(jsml.header(_header));
 	
 	for (var i =0; i < num_records; i++) {
 		var new_entry = {};
-		
+		//For example:
+		//name = temperature
+		//value = [0,100]
+		//Check the value type - is it a number? is it an array? is it an array of numbers or strings?
+		//		If it's a string do the stuff in 
+		//			else if (typeof value[0] === "string") {
+		//if it's a number in the array, do math random generation between [0] and [1] of the array.
+		//	min = array[0], max = array[1]
+		//	range = max - min
+		//	(Math.random() * range) + min
 		$.each(_header, function(name, property) {
 			var value = property;
+			
+			//This value check statement is a bit of a hack. Basically, array objects in javascript
+			//have a length property. by doing if (value.length) I'm checking if the value object is an array some what indirectly.
+			//If it is an array, I push the first value of property into the value variable. Then below I do typchecks on the value variable
+			//and make decisions based on this.
 			if (value.length)
 				value = property[0];
 			if (typeof value === "number")
