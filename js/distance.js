074d815a57f3285fd02153dc9fc214e5b4ef49be
Mike Depies <depiesms22@gmail.com>
2016-03-10 18:56:31 -0600

    finished heapsort, started euclidean distance function

--- js/distance.js
+++ js/distance.js
@@ -4,12 +4,18 @@ var distance = jsml.distance = {};
 distance.euclidean = function() {
 
 	var dist = function(x1, x2, features) {
-		
+		var sum = 0;
 		for (var f in features) {
-			
+			sum += squaredDiff(x1[f], x2[f]);
 		}
 		
-		return d;
+		return Math.sqrt(sum);
 	}
+	
+	var squaredDiff = function(x1, x2) {
+		return Math.pow((x1 - x2), 2);
+	};
+	
+	return dist;
 }
 })()
--- js/sort.js
+++ js/sort.js
@@ -1,11 +1,25 @@
 jsml.sort = {};
 
-jsml.sort.heap = heap;
-var heap = function(unsortedData) {
+/**
+	Heapsort implementation - returns a new array with the data sorted in descending order.
+	
+	Notes:
+		1) Could clean up the code a bit more and look to optimize. Currently all the data is copied, so we end up with 2 arrays.
+		2) We have a function that instantiates a function object that is only utilized once. The heapsort object should parametrize it's
+			methods instead of keeping internal states.
+*/
+jsml.sort.heap = function(unsortedData) {
+	//Use the slice(0) short cut to create a shallow copy of the array.
 	var data = unsortedData.slice(0);
 	var sortedData = [];
+	
+	/**
+		Conducts the heap sort
+		@return sorted data array.
+	*/
 	var h = function() {
-		
+		h.buildMaxHeap();
+		return sortedData;
 	}
 	
 	h.element = function(index) {
@@ -33,6 +47,12 @@ var heap = function(unsortedData) {
 		return parentIndex;
 	};
 	
+	/**
+		Builds a max heap. This is the descending order sort operation.
+		After each heapify call, we swap the max value (which is located at the 0 index) 
+		with the last index in the array. Then we pop the element off the heap array into
+		the sorted array.
+	*/
 	h.buildMaxHeap = function() {
 		var len = data.length;
 		while (data.length > 1) {
@@ -43,6 +63,9 @@ var heap = function(unsortedData) {
 		
 	};
 	
+	/**
+		Performs a heap iteration.
+	*/
 	h.heapify = function() {
 		
 		for (var index = data.length -1; index > 0; index--) {
@@ -54,12 +77,18 @@ var heap = function(unsortedData) {
 		}
 	}
 	
+	/**
+		Private method to perform swap of two elements.
+		
+		Note:
+			Should take in the array it's swapping too?
+	*/
 	var swap = function(i, j) {
 		var temp = data[i];
 		data[i] = data[j];
 		data[j] = temp;
 	};
 	
-	return h;
+	return h();
 };
 
